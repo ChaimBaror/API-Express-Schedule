9b6fdf31d7c546c5ea70272f68217e74adb2c8ec
Chaim Bar-or <chaimb@ravtech.local>
2024-03-14 18:24:50 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
 import express from "express";
 import userRoute from "./Routes/user.route";
 import timetableRoute from "./Routes/timetable";
-import { connectMongo } from "./config";
+import { run } from "./config";
 
-connectMongo()
+// Initialize Express application
 const app = express();
 
+// Middleware for parsing URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
+// Middleware for parsing JSON bodies
 app.use(express.json());
 
+// Routes
+app.use('/user', userRoute); // User-related routes
+app.use('/api', timetableRoute); // Timetable-related routes
 
-app.use('/user', userRoute)
-app.use('/api', timetableRoute)
+// Define the port for the server to listen on
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`)
-})
+// Connect to the database
+run().catch(console.error);
 
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
